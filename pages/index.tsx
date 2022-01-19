8049d90d96eb47cdb1b4aee5212eb54e3807ef67
Martin Mwangi <mwangimartin1904@gmail.com>
2022-01-19 22:21:50 +0300

    Update query and blog card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/Header";
 
-export default function Home() {
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typings";
+
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({ posts }: Props) {
+  console.log(posts);
+
   return (
     <div className=" max-w-7xl mx-auto">
       <Head>
@@ -33,6 +43,48 @@ export default function Home() {
       </div>
 
       {/* Posts */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="border overflow-hidden rounded-lg group cursor-pointer">
+              <img className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" src={urlFor(post.mainImage).url()!} alt={post.title} />
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description.substring(0, 30) +"..."} by {post.author?.name}
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author?.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const query = `*[_type=="post"]{
+    _id,
+    title,
+    author->{
+    name,
+    image
+    },
+    description,
+    mainImage,
+    slug
+  }`;
+  const posts = await sanityClient.fetch(query);
+  return {
+    props: {
+      posts,
+    },
+  };
+};
