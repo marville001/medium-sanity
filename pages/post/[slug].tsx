3e648fc7626c749894c52ab5d8b9be1357bc75eb
Martin Mwangi <mwangimartin1904@gmail.com>
2022-01-23 14:01:59 +0300

    Update comment form and api

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,12 +4,40 @@ import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
 
 import PortableText from "react-portable-text";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 interface Props {
   post: Post;
 }
 
-const Post = ({ post }: Props) => {
+interface IFormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
+
+const Post_ = ({ post }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log({datata: data});
+    
+    fetch("/api/createComment", {
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((resData) => console.log({resData}))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <main>
       <Header />
@@ -42,14 +70,17 @@ const Post = ({ post }: Props) => {
           <PortableText
             dataset={process.env.NEXT_PUBLIC_SANITY_DATASET || "production"}
             projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
-            content={post.body}
-            className=""
+            content={post?.body}
+            className="w-full"
             serializers={{
               h1: (props: any) => {
-                <h1 className="text-2xl font-bold my-5" {...props} />;
+                return <h1 className="text-2xl font-bold my-5" {...props} />;
               },
               h2: (props: any) => {
-                <h1 className="text-xl font-bold my-5" {...props} />;
+                return <h1 className="text-xl font-bold my-5" {...props} />;
+              },
+              normal: (props: any) => {
+                return <p className="text-md font-light my-2" {...props} />;
               },
               mainImage: (props: any) => {
                 <img className="w-full" {...props} />;
@@ -58,9 +89,11 @@ const Post = ({ post }: Props) => {
                 <li className="text-2xl font-bold my-5">{children}</li>;
               },
               link: ({ href, children }: any) => {
-                <a href={href} className="text-blue-500 hover:underline">
-                  {children}
-                </a>;
+                return (
+                  <a href={href} className="text-blue-500 hover:underline">
+                    {children}
+                  </a>
+                );
               },
             }}
           />
@@ -68,15 +101,22 @@ const Post = ({ post }: Props) => {
       </article>
       <hr className="max-w-lg my-5 border mx-auto border-yellow-500" />
 
-      <form className="flex flex-col p-5 my-10 max-w-2xl mx-auto">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col p-5 my-10 max-w-2xl mx-auto"
+      >
         <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
         <h4 className="text-3xl font-bold">Leave a comment below</h4>
         <hr className="py-3 mt-2" />
 
+        <input {...register("_id")} name="_id" type="hidden" value={post._id} />
+
         <label className="block mb-5">
           <span className="text-gray-700 block mb-2">Name</span>
           <input
-            className="shadow border rounded py-2 px-3 form-inut mt-1 w-full ring-yellow-500 focus:ring-1 outline-none"
+            {...register("name", { required: true })}
+            name="name"
+            className="shadow border rounded py-2 px-3 form-input mt-1 w-full ring-yellow-500 focus:ring-1 outline-none"
             type="text"
             placeholder="Name"
           />
@@ -84,7 +124,9 @@ const Post = ({ post }: Props) => {
         <label className="block mb-5">
           <span className="text-gray-700 block mb-2">Email</span>
           <input
-            className="shadow border rounded py-2 px-3 form-inut mt-1 w-full ring-yellow-500 focus:ring-1 outline-none"
+            {...register("email", { required: true })}
+            name="email"
+            className="shadow border rounded py-2 px-3 form-input mt-1 w-full ring-yellow-500 focus:ring-1 outline-none"
             type="email"
             placeholder="Email"
           />
@@ -92,17 +134,38 @@ const Post = ({ post }: Props) => {
         <label className="block mb-5">
           <span className="text-gray-700 block mb-2">Comment</span>
           <textarea
-            className="shadow border rounded py-2 px-3 form-inut mt-1 w-full ring-yellow-500 focus:ring-1 outline-none"
+            {...register("comment", { required: true })}
+            name="comment"
+            className="shadow border rounded py-2 px-3 form-textarea mt-1 w-full ring-yellow-500 focus:ring-1 outline-none"
             placeholder="Name"
             rows={8}
           ></textarea>
         </label>
+
+        <div className=" flex flex-col p-5">
+          {errors.name && (
+            <span className="text-red-500"> - The Name Field is required</span>
+          )}
+          {errors.email && (
+            <span className="text-red-500"> - The Email Field is required</span>
+          )}
+          {errors.comment && (
+            <span className="text-red-500">
+              {" "}
+              - The Comment Field is required
+            </span>
+          )}
+        </div>
+        <input
+          type="submit"
+          className="bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer"
+        />
       </form>
     </main>
   );
 };
 
-export default Post;
+export default Post_;
 
 export const getStaticPaths = async () => {
   const query = `*[_type=="post"]{
